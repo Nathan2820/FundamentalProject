e2621816fac7129f1a0d70a421b5fe05b6952a23
Nathan Orsmond <orsmondn@hotmail.com>
2020-08-31 16:32:14 +0100

    Added functionality to PlayerController.java.

--- src/main/java/com/qa/fantasy/rest/PlayerController.java
+++ src/main/java/com/qa/fantasy/rest/PlayerController.java
@@ -1,12 +1,14 @@
 package com.qa.fantasy.rest;
 
+import com.qa.fantasy.domain.Player;
 import com.qa.fantasy.dto.PlayerDTO;
 import com.qa.fantasy.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
@@ -24,5 +26,26 @@ public class PlayerController {
         return ResponseEntity.ok(this.playerService.viewAllPlayers());
     }
 
+    @PostMapping("/createPlayer")
+    public ResponseEntity<PlayerDTO> createPlayer(@RequestBody Player player) {
+        return new ResponseEntity<PlayerDTO>(this.playerService.createPlayer(player), HttpStatus.CREATED);
+    }
 
+    @DeleteMapping("/deletePlayer/{id}")
+    public ResponseEntity<?> deletePlayer(@PathVariable Long id) {
+        return this.playerService.deletePlayerById(id)
+                ? ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build()
+                : ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/getPlayerById/{id}")
+    public ResponseEntity<PlayerDTO> getPlayerById(@PathVariable Long id) {
+        return ResponseEntity.ok(this.playerService.findPlayerById(id));
+    }
+
+    @PutMapping("/updatePlayer")
+    public ResponseEntity<PlayerDTO> updatePlayer(@PathParam("id") Long id, @RequestBody Player player) {
+        //localhost:8080/updatePlayer?id="insertidhere"
+        return ResponseEntity.ok(this.playerService.updatePlayer(id, player));
+    }
 }
