72b2a448886cd518f53de2aa96d0e8328db307bb
Nathan Orsmond <orsmondn@hotmail.com>
2020-09-05 14:55:50 +0100

    Printing out context path on run

--- src/main/java/com/qa/fantasy/FantasyApplication.java
+++ src/main/java/com/qa/fantasy/FantasyApplication.java
@@ -2,12 +2,13 @@ package com.qa.fantasy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @SpringBootApplication
 public class FantasyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FantasyApplication.class, args);
+        System.out.println("#########" + ServletUriComponentsBuilder.fromCurrentContextPath() + "##############");
     }
-
 }
