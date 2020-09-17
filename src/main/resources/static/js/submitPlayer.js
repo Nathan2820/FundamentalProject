90db96dc8fb23420f53032fafaa201ae28bae41c
Nathan Orsmond <59610591+Nathan2820@users.noreply.github.com>
2020-09-17 11:11:25 +0100

    Update submitPlayer.js

--- src/main/resources/static/js/submitPlayer.js
+++ src/main/resources/static/js/submitPlayer.js
@@ -6,7 +6,7 @@ function submitPlayer() {
         obj[item.name] = item.value;
     }
     const req = new XMLHttpRequest();
-    req.open("POST", "http://34.89.6.97:8080/createPlayer");
+    req.open("POST", "http://34.105.160.58:8080/createPlayer");
     req.onload = () => {
         if (req.status === 200 && req.readyState === 4) {
             console.log("Server responded with: " + req.responseText);
